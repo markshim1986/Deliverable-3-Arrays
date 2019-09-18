38194f659d8f466052ad76d8e836f82b58624536
markshim1986 <mshim@mail.usf.eud>
2019-09-18 19:16:18 -0400

    This is a change

--- Deliverable 3 Arrays/Program.cs
+++ Deliverable 3 Arrays/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Author: Mark Shim
+// Date: 9/17/19
+// Comments: Creating a C# Console Application which uses an 
+// array to auto populate information
+using System;
 
 namespace Deliverable_3_Arrays
 {
@@ -6,7 +10,28 @@ namespace Deliverable_3_Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Implementing a try-catch statement to catch any exceptions
+            try
+            {
+                // Declaing my variable and the elements in my array
+
+                int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+                foreach (int i in numbers)
+                {
+                    Console.WriteLine("Element Value = " + i);
+                }
+                Console.WriteLine("\n Press any key to exit the program");
+                Console.ReadKey(true);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
+
+         
+
+    
+     
